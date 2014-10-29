a9b49f2fd4b77c0cc4b16c6f1af4cb6e7346f2de
Sebastian Clausen <info@sebastian-clausen.de>
2014-10-29 16:47:01 +0100

    bugfix

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
     summary: "Expose mongodb aggregation (mapReduce, aggregate and distinct), to a FS.Collection SERVER side only.",
     version: '1.0.0',
-    name: 'phosphoros:cfs-server-aggregation',
-    git: 'https://github.com/sclausen/meteor-mongo-server.git'
+    name: 'phosphoros:cfs-aggregation',
+    git: 'https://github.com/sclausen/cfs-aggregation.git'
 });
 
 Npm.depends({mongodb: "1.4.19"});
@@ -11,7 +11,7 @@ Package.on_use(function (api, where) {
     if(api.versionsFrom !== undefined) api.versionsFrom('METEOR@1.0');
 
     api.use('underscore', ['server']);
-    api.use('cfs:standard-packages', ['server']);
+    api.use('cfs:standard-packages@0.0.2', ['server']);
 
     api.add_files('server.js', 'server');
 });
